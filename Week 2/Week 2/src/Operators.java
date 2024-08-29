ba209b84d77455008accd5e8565b8f8a03368d31
Tresnaft <tresnaft.13@gmail.com>
2024-08-30 01:54:39 +0700

    Revert "Week 2"
    This reverts commit 4fa1f6a8340da8a357a2cd8a0a846b983fb6c340.

--- Week 2/Week 2/src/Operators.java
+++
@@ -1,8 +0,0 @@
-class Floating_Point{
-	public static void main(String[] args) {
-		double x = 92.98;
-//		int nx = (int) x;
-		int nx = (int) Math.round(x);
-		System.out.println("nx = " + nx);
-	}
-}
