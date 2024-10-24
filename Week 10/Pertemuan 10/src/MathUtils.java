cf3fa813d90f06f59900854b1a08ed1944786ff8
Tresnaft <tresnaft.13@gmail.com>
2024-10-24 13:29:50 +0700

    W10

---
+++ Week 10/Pertemuan 10/src/MathUtils.java
@@ -0,0 +1,22 @@
+// ****************************************************************
+// MathUtils.java
+//
+// Provides static mathematical utility functions.
+//
+// ****************************************************************
+public class MathUtils{
+    //-------------------------------------------------------------
+// Returns the factorial of the argument given
+//-------------------------------------------------------------
+    public static int factorial(int n) throws IllegalArgumentException{
+        if (n < 0){
+            throw new IllegalArgumentException("Integer " + n +" must be positive");
+        } else if (n > 16){
+            throw new IllegalArgumentException("Integer " + n + " must be below 17");
+        }
+        int fac = 1;
+        for (int i=n; i>0; i--)
+            fac *= i;
+        return fac;
+    }
+}
